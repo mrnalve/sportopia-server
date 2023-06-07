e0f7ff4aa2248e92dd11e8ee5a755c58a5959d30
Md Alve <mdalve1091@gmail.com>
2023-06-08 00:21:00 +0600

    manage user and save to the database

--- index.js
+++ index.js
@@ -1,11 +1,78 @@
 const express = require('express');
 const app = express()
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
 require('dotenv').config()
 
-app.get('/', (req, res)=>{
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+// mongodb code
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c9irx2a.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const userCollection = client.db('SportopiaDB').collection('Users')
+
+
+        // user collection api starts
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existUser = await userCollection.findOne(query)
+            if (existUser) {
+                return res.send({ message: 'User already exist' })
+            }
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
+        // user collection api ends
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
     res.send('Sportopia running')
 })
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('Sportopia running on the port', port);
 })
